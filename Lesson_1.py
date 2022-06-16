2abf02003e322034291b77d3ae8ff2b3e3c167de
azimax347 <the.azimax@gmail.com>
2022-06-16 17:38:10 +0700

    Типы переменных

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,6 +1,19 @@
-print("hello world") #Функция вывода
+print("Hello world")  # Функция вывода
+x = input("Номер: ")
 '''
 Для вывода
 блочного
 комментария 
 '''
+print("hello","world", "2022")
+name_1 = False # Тип bool
+name_2 = 23 # Тип int
+name_3 = 0b11001 # Двоичная система
+name_4 = 0o21 # Восьмиричная система
+name_5 = 0xD60 # Шестнадцатиричная система
+name_6 = 53.12 # Тип float
+name_7 = 4+3j # Тип complex
+name_8 = "someone" # Тип str
+name_9 = r"C:\python\name.txt"
+name_10 = f"test: {name_8} \t test_2: {name_4}" #Вставка переменных в строчку
+print(type(name_10), name_10)
